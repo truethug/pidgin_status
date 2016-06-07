09eca2c56fe9e1c4b32bafcf0ea7e2c6b5fa9790
truethug <truethug@gmail.com>
2016-06-07 11:11:31 -0400

    Create pidgin_status.sh
    initial commit

---
+++ pidgin_status.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#screen=`env|grep DISPLAY`
+screen="DISPLAY=:0.0"
+
+egrep "DTSTART|DTEND|SUMMARY|STATUS" ~/.thunderbird/Work.ics > /tmp/cleaned
+
+# remove old at jobs
+for i in `atq|awk '{print $1}'`
+do
+   atrm $i
+done
+
+while read -r line
+do
+   summary=`echo $line | tr -d '\r' | grep SUMMARY:`
+   if [ "$summary" != "" ]
+   then
+      summary=`echo $line | tr -d '\r' | cut -d: -f2-`
+      summary=`echo "$summary" | sed 's/\"/\\\"/g'`
+
+      read line
+      status=`echo $line | tr -d '\r' | cut -d: -f2-`
+
+      read line
+      startDay=`echo $line | tr -d '\r' | cut -d: -f2- | cut -dT -f1`
+      startDay=`date -d "$startDay" +%m/%d/%y`
+      startHour=`echo $line | tr -d '\r' | cut -d: -f2- | cut -dT -f2 | cut -c1-2`
+      startMin=`echo $line | tr -d '\r' | cut -d: -f2- | cut -dT -f2 | cut -c3-4`
+
+      read line
+      endDay=`echo $line | tr -d '\r' | cut -d: -f2- | cut -dT -f1`
+      endDay=`date -d "$endDay" +%m/%d/%y`
+      endTime=`echo $line | tr -d '\r' | cut -d: -f2- | cut -dT -f2 | cut -c1-4`
+      endTime=`date -d "$endTime 1 min ago" +%R`
+
+      if [ "$status" != "CANCELED" ] 
+      then
+         # Add new at jobs
+         echo "export $screen;/usr/bin/purple-remote setstatus?status=extended_away;/usr/bin/purple-remote setstatus?message=\"$summary\"  | at $startHour:$startMin $startDay"
+         echo "export $screen;/usr/bin/purple-remote setstatus?status=extended_away;/usr/bin/purple-remote setstatus?message=\"$summary\"" | at $startHour:$startMin $startDay
+         echo "export $screen;/usr/bin/purple-remote setstatus?status=available;/usr/bin/purple-remote setstatus?message=" | at $endTime $endDay
+      fi
+   fi
+done < /tmp/cleaned
+rm /tmp/cleaned
+
+minute=`date|cut -d: -f2`
+if [ `expr $minute % 5` -eq 0 ]
+then
+   echo "/home/phic/Documents/pidgin_status.sh" | at now + 4 minutes
+else
+   echo "/home/phic/Documents/pidgin_status.sh" | at now + 5 minutes
+fi
