5a654b75f433f330c959a4c43e53d2c8d35b139c
truethug <truethug@gmail.com>
2016-06-20 10:37:22 -0400

    Update pidgin_status.sh
    adding timezone adjustments

--- pidgin_status.sh
+++ pidgin_status.sh
@@ -23,27 +23,30 @@ do
       status=`echo $line | tr -d '\r' | cut -d: -f2-`
 
       read line
+      TZ=`echo $line | cut -d= -f2| cut -d: -f1`
       startDay=`echo $line | tr -d '\r' | cut -d: -f2- | cut -dT -f1`
       startDay=`date -d "$startDay" +%m/%d/%y`
       startHour=`echo $line | tr -d '\r' | cut -d: -f2- | cut -dT -f2 | cut -c1-2`
       startMin=`echo $line | tr -d '\r' | cut -d: -f2- | cut -dT -f2 | cut -c3-4`
-
+      startTime=`date --date="TZ=\"$TZ\" $startHour:$startMin" | awk '{print $4}' | cut -d: -f1,2` 
+    
       read line
       endDay=`echo $line | tr -d '\r' | cut -d: -f2- | cut -dT -f1`
       endDay=`date -d "$endDay" +%m/%d/%y`
       endTime=`echo $line | tr -d '\r' | cut -d: -f2- | cut -dT -f2 | cut -c1-4`
+      endTime=`date --date="TZ=\"$TZ\" $endTime" | awk '{print $4}' | cut -d: -f1,2`
       endTime=`date -d "$endTime 1 min ago" +%R`
 
       if [ "$status" != "CANCELED" ] 
       then
          # Add new at jobs
-         echo "export $screen;/usr/bin/purple-remote setstatus?status=extended_away;/usr/bin/purple-remote setstatus?message=\"$summary\"  | at $startHour:$startMin $startDay"
-         echo "export $screen;/usr/bin/purple-remote setstatus?status=extended_away;/usr/bin/purple-remote setstatus?message=\"$summary\"" | at $startHour:$startMin $startDay
+         echo "export $screen;/usr/bin/purple-remote setstatus?status=extended_away;/usr/bin/purple-remote setstatus?message=\"$summary\"  | at $startTime $startDay"
+         echo "export $screen;/usr/bin/purple-remote setstatus?status=extended_away;/usr/bin/purple-remote setstatus?message=\"$summary\"" | at $startTime $startDay
          echo "export $screen;/usr/bin/purple-remote setstatus?status=available;/usr/bin/purple-remote setstatus?message=" | at $endTime $endDay
       fi
    fi
 done < /tmp/cleaned
-rm /tmp/cleaned
+#rm /tmp/cleaned
 
 minute=`date|cut -d: -f2`
 if [ `expr $minute % 5` -eq 0 ]
