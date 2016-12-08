d154ae182c7a2bf7104fdddc4e41368512cec77a
Charlie Phillips <phic@netapp.com>
2016-12-08 14:42:46 -0500

    adjusting for microsoft out of order

--- pidgin_status.sh
+++ pidgin_status.sh
@@ -9,6 +9,71 @@ SCRIPT_HOME=$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )
 #screen="DISPLAY=:0.0"
 screen=$1
 
+function getSummary() 
+{
+   summary=`echo $line | tr -d '\r' | cut -d: -f2-`
+   summary=`echo "$summary" | sed 's/\"/\\\"/g'`
+}
+
+function getStatus()
+{
+   status=`echo $line | tr -d '\r' | cut -d: -f2-`
+}
+
+function getStart()
+{
+   TZ=`echo $line | cut -d= -f2| cut -d: -f1`
+   TZ=`echo $TZ | sed 's/\"//g'`
+   startDay=`echo $line | tr -d '\r' | cut -d: -f2- | cut -dT -f1`
+   startDay=`date -d "$startDay" +%m/%d/%y`
+   startTime=`echo $line | tr -d '\r' | cut -d: -f2- | cut -dT -f2 | cut -c1-4`
+   if [ "$Microsoft" == "" ]
+   then
+      startTime=`date --date="TZ=\"$TZ\" $startTime" | awk '{print $4}' | cut -d: -f1,2`
+   fi
+}
+
+function getEnd()
+{
+   TZ=`echo $line | cut -d= -f2| cut -d: -f1`
+   TZ=`echo $TZ | sed 's/\"//g'`
+   endDay=`echo $line | tr -d '\r' | cut -d: -f2- | cut -dT -f1`
+   endDay=`date -d "$endDay" +%m/%d/%y`
+   endTime=`echo $line | tr -d '\r' | cut -d: -f2- | cut -dT -f2 | cut -c1-4`
+   if [ "$Microsoft" == "" ]
+   then
+      endTime=`date --date="TZ=\"$TZ\" $endTime" | awk '{print $4}' | cut -d: -f1,2`
+   fi
+   # end one minute early to avoid conflict with next item starting at the same time
+   endTime=`date -d "$endTime 1 min ago" +%R`
+}
+
+function getInfo()
+{
+   if [ "`echo $line | grep BEGIN:VEVENT`" == "" ]
+   then
+      if [ "`echo $line | grep ^SUMMARY`" != "" ]
+      then
+         getSummary
+         echo $summary
+      elif [ "`echo $line | grep STATUS:`" != "" ]
+      then
+         getStatus
+         echo $status
+      elif [ "`echo $line | grep ^DTSTART`" != "" ]
+      then
+         getStart
+         echo $startDay $startTime
+      elif [ "`echo $line | grep ^DTEND`" != "" ]
+      then
+         getEnd
+         echo $endDay $endTime
+      fi
+   else
+      stop="true"
+   fi
+}
+
 # If this exists use webdav
 if [ -f ~/.ssh/webdav ]
 then
@@ -20,101 +85,71 @@ then
       # https://server.com:port/path/calendar.ics
       server=$line
       echo $server
-   curl -k --user "$secure" "$server" > ~/.thunderbird/Work.ics
+      curl -k --user "$secure" "$server" > ~/.thunderbird/Work.ics
    done < ~/.ssh/webdav
 fi
 
-egrep "DTSTART|DTEND|SUMMARY|STATUS" ~/.thunderbird/Work.ics > /tmp/cleaned
+egrep "DTSTART|DTEND|SUMMARY|STATUS|BEGIN:VEVENT" ~/.thunderbird/Work.ics > /tmp/cleaned
 m1=`md5sum /tmp/cleaned | awk '{print $1}'`
 m2=`md5sum /tmp/cleaned2 | awk '{print $1}'`
 
 if [ "$m1" != "$m2" ]
 then
 
-# remove old at jobs
-for i in `atq|awk '{print $1}'`
-do
-   atrm $i
-done
+   Microsoft=`grep X-MICROSOFT-CDO-BUSYSTATUS: /tmp/cleaned`
 
-Microsoft=`grep X-MICROSOFT-CDO-BUSYSTATUS: /tmp/cleaned`
-if [ "$Microsoft" == "" ]
-then
+   # remove old at jobs
+   for i in `atq|awk '{print $1}'`
+   do
+      atrm $i
+   done
+
    while read -r line
    do
-      summary=`echo $line | tr -d '\r' | grep ^SUMMARY`
-      if [ "$summary" != "" -a "`echo $summary | grep -i Canceled`" == "" ]
+      summary=""
+      status=""
+      startTime=""
+      endTime=""
+      stop=""
+      begin=`echo $line | tr -d '\r' | grep BEGIN:VEVENT`
+      if [ "$begin" != "" ]
       then
-         summary=`echo $line | tr -d '\r' | cut -d: -f2-`
-         summary=`echo "$summary" | sed 's/\"/\\\"/g'`
+         echo ""
+         echo ""
+         echo ""
 
          read line
-         status=`echo $line | tr -d '\r' | cut -d: -f2-`
-         echo Summary: $summary Status: $status
+         getInfo
 
-         read line
-         TZ=`echo $line | cut -d= -f2| cut -d: -f1`
-         startDay=`echo $line | tr -d '\r' | cut -d: -f2- | cut -dT -f1`
-         startDay=`date -d "$startDay" +%m/%d/%y`
-         startTime=`echo $line | tr -d '\r' | cut -d: -f2- | cut -dT -f2 | cut -c1-4`
-         startAt=`date --date="TZ=\"$TZ\" $startTime" | awk '{print $4}' | cut -d: -f1,2` 
-    
-         read line
-         endDay=`echo $line | tr -d '\r' | cut -d: -f2- | cut -dT -f1`
-         endDay=`date -d "$endDay" +%m/%d/%y`
-         endTime=`echo $line | tr -d '\r' | cut -d: -f2- | cut -dT -f2 | cut -c1-4`
-         endTime=`date --date="TZ=\"$TZ\" $endTime" | awk '{print $4}' | cut -d: -f1,2`
-         # end one minute early to avoid conflict with next item starting at the same time
-         endTime=`date -d "$endTime 1 min ago" +%R`
+         if [ "$stop" != "true" ]
+         then
+            read line
+            getInfo
+         fi 
 
-         if [ "$status" != "TENTATIVE" -a "$status" != "CANCELED" -a "$startTime" != "" ] 
+
+         if [ "$stop" != "true" ]
+         then
+            read line
+            getInfo
+         fi 
+
+         if [ "$stop" != "true" ]
+         then
+            read line
+            getInfo
+         fi 
+
+         if [ "$status" != "TENTATIVE" -a "$status" != "CANCELED" -a "$startTime" != "" -a "`echo $summary | grep -i Canceled`" == "" ] 
          then
             # Add new at jobs
-            echo "export $screen;/usr/bin/purple-remote setstatus?status=extended_away;/usr/bin/purple-remote setstatus?message=\"$summary\"  | at $startAt $startDay"
-            echo "export $screen;/usr/bin/purple-remote setstatus?status=extended_away;/usr/bin/purple-remote setstatus?message=\"$summary\"" | at $startAt $startDay
+            echo "export $screen;/usr/bin/purple-remote setstatus?status=extended_away;/usr/bin/purple-remote setstatus?message=\"$summary\"  | at $startTime $startDay"
+            echo "export $screen;/usr/bin/purple-remote setstatus?status=extended_away;/usr/bin/purple-remote setstatus?message=\"$summary\"" | at $startTime $startDay
             echo "export $screen;/usr/bin/purple-remote setstatus?status=available;/usr/bin/purple-remote setstatus?message=" | at $endTime $endDay
          fi
       fi
    done < /tmp/cleaned
-else # Microsoft format
-   while read -r line
-   do
-      summary=`echo $line | tr -d '\r' | grep ^SUMMARY`
-      if [ "$summary" != "" -a "`echo $summary | grep -i Canceled`" == "" ]
-      then
-         summary=`echo $line | tr -d '\r' | cut -d: -f2-`
-         summary=`echo "$summary" | sed 's/\"/\\\"/g'`
-
-         read line
-         TZ=`echo $line | cut -d= -f2| cut -d: -f1`
-         startDay=`echo $line | tr -d '\r' | cut -d: -f2- | cut -dT -f1`
-         startDay=`date -d "$startDay" +%m/%d/%y`
-         startTime=`echo $line | tr -d '\r' | cut -d: -f2- | cut -dT -f2 | cut -c1-4`
-         startAt=`date --date="TZ=\"$TZ\" $startTime" | awk '{print $4}' | cut -d: -f1,2` 
-    
-         read line
-         endDay=`echo $line | tr -d '\r' | cut -d: -f2- | cut -dT -f1`
-         endDay=`date -d "$endDay" +%m/%d/%y`
-         endTime=`echo $line | tr -d '\r' | cut -d: -f2- | cut -dT -f2 | cut -c1-4`
-         endTime=`date --date="TZ=\"$TZ\" $endTime" | awk '{print $4}' | cut -d: -f1,2`
-         # end one minute early to avoid conflict with next item starting at the same time
-         endTime=`date -d "$endTime 1 min ago" +%R`
-
-         read line
-         status=`echo $line | tr -d '\r' | cut -d: -f2-`
-         echo Summary: $summary Status: $status
-
-         if [ "$status" != "TENTATIVE" -a "$status" != "CANCELED" -a "$startTime" != "" ] 
-         then
-            # Add new at jobs
-            echo "export $screen;/usr/bin/purple-remote setstatus?status=extended_away;/usr/bin/purple-remote setstatus?message=\"$summary\"  | at $startAt $startDay"
-            echo "export $screen;/usr/bin/purple-remote setstatus?status=extended_away;/usr/bin/purple-remote setstatus?message=\"$summary\"" | at $startAt $startDay
-            echo "export $screen;/usr/bin/purple-remote setstatus?status=available;/usr/bin/purple-remote setstatus?message=" | at $endTime $endDay
-         fi
-      fi
-   done < /tmp/cleaned
-fi
-cp /tmp/cleaned /tmp/cleaned2
+   mv /tmp/cleaned /tmp/cleaned2
 fi
 
 # Run the script again every 5 min ( 1 min earlier if on the 0 or 5 to avoid removing at jobs for current minute
@@ -125,3 +160,4 @@ then
 else
    echo "$SCRIPT_HOME/pidgin_status.sh $screen" | at now + 5 minutes
 fi
+
