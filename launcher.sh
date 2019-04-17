aea7d891582571c26df1b68f3a5c791960007792
Charlie Phillips <charlphi@cisco.com>
2019-04-17 11:58:48 -0400

    adding display fix

--- launcher.sh
+++ launcher.sh
@@ -6,4 +6,4 @@ echo `date -d now +%Y%m%d%H%M%S` > /tmp/.awaytime
 
 $SCRIPT_HOME/pidgin_status.sh $display
 
-dbus-monitor --system "type='signal',sender='org.freedesktop.login1',path='/org/freedesktop/login1/seat/seat0',interface='org.freedesktop.DBus.Properties',member='PropertiesChanged'" | grep --line-buffered "ActiveSession" | while read line; do $SCRIPT_HOME/away.sh $display; done
+dbus-monitor --system "type='signal',sender='org.freedesktop.login1',path='/org/freedesktop/login1/seat/seat0',interface='org.freedesktop.DBus.Properties',member='PropertiesChanged'" | grep --line-buffered "ActiveSession" | while read line; do $SCRIPT_HOME/away.sh $display;xfce4-panel -r; xmodmap ~/.Xmodmap; kill $(ps aux | grep fce4-display-settings\ --minimal | awk '{print $2}'); done
